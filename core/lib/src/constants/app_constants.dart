1d9e354ed536198a0d4f688e148baf63f1443b51
aliaksandr.huzich <aliaksandr.huzich@innowise.com>
2024-11-27 14:11:55 +0300

    [network-checker-service] Add network checker service Fix constants names

--- core/lib/src/constants/app_constants.dart
+++ core/lib/src/constants/app_constants.dart
@@ -1,3 +1,5 @@
 abstract final class AppConstants {
-  static const String appThemeStorageFlag = 'app_theme';
+  static const String APP_THEME_STORAGE_FLAG = 'app_theme';
+  static const Duration INTERNET_OBSERVING_INTERVAL = Duration(seconds: 7);
+  static const String INTERNET_OBSERVING_URL = 'https://www.google.com/';
 }

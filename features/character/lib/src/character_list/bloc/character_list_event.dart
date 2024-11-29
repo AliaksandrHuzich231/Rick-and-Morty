061002ed5b6838cf62ebc3a75b16edd5ccabf775
aliaksandr.huzich <aliaksandr.huzich@innowise.com>
2024-11-29 17:02:56 +0300

    [character-list] Remove event

--- features/character/lib/src/character_list/bloc/character_list_event.dart
+++ features/character/lib/src/character_list/bloc/character_list_event.dart
@@ -2,8 +2,6 @@ part of 'character_list_bloc.dart';
 
 sealed class CharacterListEvent {}
 
-final class InitialLoad extends CharacterListEvent {}
-
 final class LoadMoreCharacters extends CharacterListEvent {}
 
 final class MoveToDetailsPage extends CharacterListEvent {}
